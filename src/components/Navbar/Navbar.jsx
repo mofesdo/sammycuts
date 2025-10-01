496c315f30c00d731bc9dd051a8a81122dd12569
Modesto <mofesdo.dev@gmail.com>
2025-09-30 21:17:14 -0700

    Feat: Updated Navbar to include links to future sections

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,9 +4,21 @@ function Navbar() {
     <nav className="navbar">
       <h1>Sammy Cuts</h1>
       <ul className="nav_links">
-        <li><a href="#home">Home</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#contact">Contact</a></li>
+        <li>
+          <a href="#home">Home</a>
+        </li>
+        <li>
+          <a href="#booking">Booking</a>
+        </li>
+        <li>
+          <a href="#gallery">Gallery</a>
+        </li>
+        <li>
+          <a href="#reviews">Reviews</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
       </ul>
     </nav>
   );
